dd4283262e6bbddb918aa3b8801e177e8b09d5ec
bgrando24 <bgrando24@gmail.com>
2025-08-13 22:13:23 +1000

    building out functions to use the emby API

--- src/main.py
+++ src/main.py
@@ -18,10 +18,41 @@ if EMBY_API_KEY is None:
     exit(1)
 
 # check emby server can be reached
-get_base = requests.get(BASE_DOMAIN)
+get_base = requests.get(f"{BASE_DOMAIN}/System/Ping?api_key={EMBY_API_KEY}")
 if not get_base.ok:
     print(f"Error: Emby server at {BASE_DOMAIN} is unavailable. Status code: {get_base.status_code}", file=sys.stderr)
     exit(1)
 else:
     print(f"Emby server available at [{BASE_DOMAIN}]! Status: {get_base.status_code}")
+
+
+# --- user functions ---
+def get_all_emby_users() -> dict[str, str]:
+    """
+    Fetch all Emby users (including hidden users)
+
+    Returns:
+        dict: keys = usernames, values = Emby user ID
+    """
+    response = requests.get(f"{BASE_DOMAIN}/Users/Query?IsHidden=true&api_key={EMBY_API_KEY}")
+    response.raise_for_status()
+    get_users = response.json()
+    users: dict[str, str] = {}
+    for user in get_users["Items"]:
+        users[user["Name"]] = user["Id"]
+    return users
+
+
+def get_user_watch_hist(user_id: str, num_days: int, is_aggregated=False,):
+    
+    """
+    Fetch the watch history for a user
+    Args:
+        user_id (str): User's Emby ID
+        num_days (int): The number of days to look back for watch history
+        is_aggregated (bool, optional): Whether to aggregate the data, defaults to False
+    Returns:
+        dict: The user's watch history data as a JSON-decoded dictionary
+    """
+    return requests.get(f"{BASE_DOMAIN}/user_usage_stats/UserPlaylist?user_id={user_id}&aggregate_data={is_aggregated}&days={num_days}&api_key={EMBY_API_KEY}'").json()
 

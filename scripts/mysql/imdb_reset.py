ae212e0bb7dbe1613d7b6aaaa1a3ccca26d24806
bgrando24 <bgrando24@gmail.com>
2025-09-23 12:52:24 +1000

    adding debug statements

--- scripts/mysql/imdb_reset.py
+++ scripts/mysql/imdb_reset.py
@@ -72,7 +72,22 @@ try:
 
     # forces refresh of stale row counts
     curs.execute("ANALYZE TABLE crew_staging, genres_staging, directors, episodes, genres, persons, ratings, titles, writers, roles;")
-    print(curs.fetchall())
+    print(f"{curs.fetchall()}\n\n")
+    db.commit()
+
+    curs.execute("""
+        SELECT 'crew_staging' table, COUNT(*) row_count FROM crew_staging
+        UNION ALL SELECT 'genres_staging', COUNT(*) FROM genres_staging
+        UNION ALL SELECT 'directors', COUNT(*) FROM directors
+        UNION ALL SELECT 'episodes', COUNT(*) FROM episodes
+        UNION ALL SELECT 'genres', COUNT(*) FROM genres
+        UNION ALL SELECT 'persons', COUNT(*) FROM persons
+        UNION ALL SELECT 'ratings', COUNT(*) FROM ratings
+        UNION ALL SELECT 'titles', COUNT(*) FROM titles
+        UNION ALL SELECT 'writers', COUNT(*) FROM writers
+        UNION ALL SELECT 'roles', COUNT(*) FROM roles
+    """)
+    print(f"{curs.fetchall()}\n\n")
     db.commit()
 except MySQLError as e:
     print(f"ERROR: Unable to reset database: {e}", file=sys.stderr)

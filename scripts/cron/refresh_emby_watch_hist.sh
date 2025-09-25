58c8ca8d35b63695bfc2afa5ce278c6366d0c091
bgrando24 <bgrando24@gmail.com>
2025-09-25 15:07:11 +1000

    comments

--- scripts/cron/refresh_emby_watch_hist.sh
+++ scripts/cron/refresh_emby_watch_hist.sh
@@ -1,1 +1,5 @@
-#!/usr/bin/sh
+#!/bin/sh
+
+# This script adds/removes a cron job to the executing machine, that runs a DAILY fetch of the Emby
+# 	user watch history and stores it into the local SQLite database.
+# Under the hood, this runs a the "{SCRIPT NAME}.py 
